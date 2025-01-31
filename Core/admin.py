cbb45e91676851895615b1a777746108924a99f1
Nima Bayat <bayatnima343@gmail.com>
2025-01-31 16:05:38 -0500

    core app completed.

--- Core/admin.py
+++ Core/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Image
 
 # Register your models here.
+
+@admin.register(Image)
+class ImageAdmin(admin.ModelAdmin):
+
+    list_display = ('Image' ,'alt_text', 'create_at')
+    list_filter = ('create_at',)
+    search_fields = ('alt_text',)
--- Core/models.py
+++ Core/models.py
@@ -1,3 +1,31 @@
-from django.db import models
+from django.db import models 
+from django.utils.translation import gettext_lazy as _
+import uuid
+
+
 
 # Create your models here.
+
+class BaseModel(models.Model):
+
+    id = models.UUIDField(primary_key=True , default=uuid.uuid4 , editable= False)
+    create_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+
+        abstract = True
+
+
+    
+
+class Image(BaseModel):
+
+    alt_text = models.CharField(_('Alternative Text'), max_length=50 , blank=True, null=True)
+    Image = models.ImageField(_('Image'), upload_to='images/' )
+    
+    class Meta:
+
+        verbose_name = _('Image')
+        verbose_name_plural = _('Images')
+
