94ea5ecda8d9b52547eb9f100768441a073c63ff
Nima Bayat <bayatnima343@gmail.com>
2025-01-31 17:09:15 -0500

    working on account model(model,urls,admin,).admin panel for thi app completed.

--- Account/admin.py
+++ Account/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import User 
 
 # Register your models here.
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    
+    list_display = ('username' ,'email','phone_number','create_at')
+    list_filter = ('create_at',)
+    search_fields = ('username',)
+
+
+
--- Account/models.py
+++ Account/models.py
@@ -1,3 +1,32 @@
-from django.db import models
+from django.db import models 
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
+from Core.models import BaseModel 
+from django.urls import reverse
 
 # Create your models here.
+
+class User(BaseModel , AbstractUser):
+     
+    phone_number = models.CharField(_('Phone Number'), max_length=20, blank=True )
+    birthday = models.DateField(_('Birthday') , null=True , blank=True)
+    bio = models.CharField(_('Biography') , max_length=300 , blank=True)
+    email = models.EmailField(_("Email"), max_length=254)
+    profile_image = models.ForeignKey('Core.Image' , on_delete= models.SET_DEFAULT ,
+                                        null=True , blank=True ,
+                                        related_name= 'user_image',
+                                        default= None)
+   
+
+
+
+    class Meta:
+            
+        verbose_name = _('User')
+        verbose_name_plural = _('Users')
+
+
+
+
+    
+
