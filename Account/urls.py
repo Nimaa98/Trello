b7ff5b1b7acc90a7491becd8dddd0ab8dcae5522
Nima Bayat <bayatnima343@gmail.com>
2025-02-01 09:19:27 -0500

    account app completed by adding serializer and router.

--- Account/urls.py
+++ Account/urls.py
@@ -1,6 +1,18 @@
-from django.urls import path
+from django.urls import path , include
+from .views import UserView
+from rest_framework.routers import SimpleRouter
+
+
+
+
+router = SimpleRouter()
+router.register("profile",UserView,basename="profile")
+
+
+
 
 app_name = 'Account'
 urlpatterns = [
+        path("", include(router.urls)),
 
 ]
--- Account/views.py
+++ Account/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework import status, generics, viewsets
+from rest_framework.decorators import action
+from rest_framework.views import APIView
+from rest_framework.request import Request
+from rest_framework.response import Response
+from .models import User
+from .serializers import UserSerializer
+
 
 # Create your views here.
+
+
+class UserView(viewsets.ModelViewSet):
+
+    lookup_field = 'id'
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
